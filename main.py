739fabade88d20a516a02327e74134dbbeea655a
Rohith__3330 <krohithmanikanta3@gmail.com>
2023-04-20 22:21:13 +0530

    final

--- main.py
+++ main.py
@@ -116,5 +116,5 @@ def hi(data:GAN):
         "image" : "image"
     }
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=5000)
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='127.0.0.1', port=5000)
